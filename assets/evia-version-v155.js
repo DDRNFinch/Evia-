7f7f026297e44a5769f5b17c3e9a83bcef0adf01
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-24 11:02:11 +0100

    Set Evia release to 156

--- assets/evia-version-v155.js
+++ assets/evia-version-v155.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";const meta=document.querySelector('meta[name="evia-app-version"]');if(meta)meta.setAttribute("content","155");window.EviaAppVersion=155})();
+(()=>{"use strict";const meta=document.querySelector('meta[name="evia-app-version"]');if(meta)meta.setAttribute("content","156");window.EviaAppVersion=156})();
