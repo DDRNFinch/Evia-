35327db83fd9082f1b9877c20230f8b472dcea40
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-26 19:07:53 +0100

    Add Evia v227 version marker

---
+++ assets/evia-version-v227.js
@@ -0,0 +1,5 @@
+(()=>{
+"use strict";
+const VERSION=227;
+const meta=document.querySelector('meta[name="evia-app-version"]');if(meta)meta.setAttribute("content","227");window.EviaAppVersion=227;
+})();
