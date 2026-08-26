605789fb465d49d56065547a7fc53567c1d068fe
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-26 23:53:28 +0100

    Load Pulse review snapshots in v234

--- assets/evia-version-v234.js
+++ assets/evia-version-v234.js
@@ -1,1 +1,8 @@
-(()=>{"use strict";const VERSION=234;const meta=document.querySelector('meta[name="evia-app-version"]');if(meta)meta.setAttribute("content","234");window.EviaAppVersion=234})();
+(()=>{
+"use strict";
+const VERSION=234;
+const meta=document.querySelector('meta[name="evia-app-version"]');if(meta)meta.setAttribute("content","234");window.EviaAppVersion=234;
+function load(src,key){if(document.querySelector(`script[data-${key}]`))return;const s=document.createElement("script");s.src=src;s.async=false;s.dataset[key]="1";document.head.appendChild(s)}
+load("./assets/evia-review-snapshots-v234.js?v=234","eviaReviewSnapshotsV234");
+load("./assets/evia-review-snapshot-hook-v234.js?v=234","eviaReviewSnapshotHookV234");
+})();
