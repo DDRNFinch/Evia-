fb2000e5c774990ab261c7a156a9940eedb87c53
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-26 19:52:31 +0100

    Add Evia v230 version marker

---
+++ assets/evia-version-v230.js
@@ -0,0 +1,5 @@
+(()=>{
+"use strict";
+const VERSION=230;
+const meta=document.querySelector('meta[name="evia-app-version"]');if(meta)meta.setAttribute("content","230");window.EviaAppVersion=230;
+})();
